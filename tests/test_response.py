cb84c000debe6c8ded54bbd1f39b394561a432f5
rendrend <133209983+rendrend@users.noreply.github.com>
2023-06-09 22:23:41 +0200

    Update test_response.py

--- tests/test_response.py
+++ tests/test_response.py
@@ -1,6 +1,6 @@
 import requests
 
 def test_response_status_code():
-    response = requests.get('https://api.weatherapi.com/v1/current.json?key=689649eb6e934beaa8591200230805&q=New%20York')
+    response = requests.get('https://api.weatherapi.com/v1/current.json?key=689685200230805&q=New%20York')
     
     assert response.status_code == 200
