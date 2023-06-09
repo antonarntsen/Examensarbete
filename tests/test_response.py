ba58735c4c251b9544e9f97537adeb7eaf0bb2ea
rendrend <133209983+rendrend@users.noreply.github.com>
2023-06-09 22:57:37 +0200

    test

--- tests/test_response.py
+++ tests/test_response.py
@@ -2,5 +2,5 @@ import pytest
 import requests
 
 def test_api_responsive():
-    response = requests.get('http://api.weatherapi.com/v1/current.json', params={"key": "689649230805", "q": "Stockholm", "aqi": "no"})
+    response = requests.get('http://api.weatherapi.com/v1/current.json', params={"key": "689649eb6e934beaa8591200230805", "q": "Stockholm", "aqi": "no"})
     assert response.status_code == 200
