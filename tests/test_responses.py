d0b2d576dec62a0a9896af6cd236f65721bb8b84
rendrend <133209983+rendrend@users.noreply.github.com>
2023-06-12 21:22:04 +0200

    Update test_responses.py

--- tests/test_responses.py
+++ tests/test_responses.py
@@ -49,6 +49,50 @@ sample_weather_cloudy = {
     }
 }
 
+sample_weather_rainy = {
+    "current": {
+        "temp_c": 15,
+        "condition": {
+            "text": "Rain",
+            "icon": "rain_icon"
+        },
+        "wind_kph": 10
+    }
+}
+
+sample_weather_overcast = {
+    "current": {
+        "temp_c": 15,
+        "condition": {
+            "text": "Overcast",
+            "icon": "overcast_icon"
+        },
+        "wind_kph": 10
+    }
+}
+
+sample_weather_cold = {
+    "current": {
+        "temp_c": -6,
+        "condition": {
+            "text": "Clear",
+            "icon": "cold_icon"
+        },
+        "wind_kph": 10
+    }
+}
+
+sample_weather_mist = {
+    "current": {
+        "temp_c": 15,
+        "condition": {
+            "text": "Mist",
+            "icon": "mist_icon"
+        },
+        "wind_kph": 10
+    }
+}
+
 @responses.activate
 def test_get_weather():
     responses.add(
@@ -57,7 +101,6 @@ def test_get_weather():
 
     assert get_weather("fake_api_key", "fake_city") == sample_weather_sunny
 
-# Test cases for different weather conditions
 def test_sunny_weather():
     with patch('app.get_weather', return_value=sample_weather_sunny):
         with app.test_client() as client:
@@ -69,3 +112,27 @@ def test_cloudy_weather():
         with app.test_client() as client:
             resp = client.post('/', data={'city': 'fake_city'})
             assert b"The shade feels nice." in resp.data
+
+def test_rainy_weather():
+    with patch('app.get_weather', return_value=sample_weather_rainy):
+        with app.test_client() as client:
+            resp = client.post('/', data={'city': 'fake_city'})
+            assert b"Grab an umbrella!" in resp.data
+
+def test_overcast_weather():
+    with patch('app.get_weather', return_value=sample_weather_overcast):
+        with app.test_client() as client:
+            resp = client.post('/', data={'city': 'fake_city'})
+            assert b"The shade feels nice." in resp.data
+
+def test_cold_weather():
+    with patch('app.get_weather', return_value=sample_weather_cold):
+        with app.test_client() as client:
+            resp = client.post('/', data={'city': 'fake_city'})
+            assert b"It's really cold out there." in resp.data
+
+def test_mist_weather():
+    with patch('app.get_weather', return_value=sample_weather_mist):
+        with app.test_client() as client:
+            resp = client.post('/', data={'city': 'fake_city'})
+            assert b"It might be hard to see." in resp.data
