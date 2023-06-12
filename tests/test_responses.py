a13cc0a19f3cf9d913f892ceb5a1f316c9da72f2
rendrend <133209983+rendrend@users.noreply.github.com>
2023-06-12 20:32:10 +0200

    Update test_responses.py

--- tests/test_responses.py
+++ tests/test_responses.py
@@ -1,7 +1,9 @@
 import pytest
 import requests
-from unittest.mock import Mock
+from unittest.mock import patch
 from app import app, get_weather
+import responses
+import json
 
 @pytest.fixture
 def client():
@@ -25,68 +27,45 @@ def test_weather_api_responsive():
     response = requests.get(base_url, params=params)
     assert response.status_code == 200
     
-def create_mock_weather(temp_c, condition_text):
-    return {
-        "current": {
-            "temp_c": temp_c,
-            "condition": {
-                "text": condition_text,
-                "icon": ""
-            },
-            "wind_kph": 5
-        }
+sample_weather_sunny = {
+    "current": {
+        "temp_c": 15,
+        "condition": {
+            "text": "Sunny",
+            "icon": "sunny_icon"
+        },
+        "wind_kph": 10
     }
+}
 
-def test_sunny_hot_weather(mocker, client):
-    mocker.patch('requests.get', return_value=Mock(ok=True))
-    requests.get.return_value.json.return_value = create_mock_weather(30, "Sunny")
-    response = client.post('/', data={"city": "test"})
-    assert b"Use sunscreen, dummy!" in response.data
+sample_weather_cloudy = {
+    "current": {
+        "temp_c": 15,
+        "condition": {
+            "text": "Cloudy",
+            "icon": "cloudy_icon"
+        },
+        "wind_kph": 10
+    }
+}
 
-def test_sunny_cold_weather(mocker, client):
-    mocker.patch('requests.get', return_value=Mock(ok=True))
-    requests.get.return_value.json.return_value = create_mock_weather(-10, "Sunny")
-    response = client.post('/', data={"city": "test"})
-    assert b"It's really cold out there." in response.data
+@responses.activate
+def test_get_weather():
+    responses.add(
+        responses.GET, 'http://api.weatherapi.com/v1/current.json',
+        json=sample_weather_sunny, status=200)
 
-def test_cloudy_weather(mocker, client):
-    mocker.patch('requests.get', return_value=Mock(ok=True))
-    requests.get.return_value.json.return_value = create_mock_weather(20, "Cloudy")
-    response = client.post('/', data={"city": "test"})
-    assert b"The shade feels nice." in response.data
+    assert get_weather("fake_api_key", "fake_city") == sample_weather_sunny
 
-def test_overcast_weather(mocker, client):
-    mocker.patch('requests.get', return_value=Mock(ok=True))
-    requests.get.return_value.json.return_value = create_mock_weather(20, "Overcast")
-    response = client.post('/', data={"city": "test"})
-    assert b"The shade feels nice." in response.data
+# Test cases for different weather conditions
+def test_sunny_weather():
+    with patch('app.get_weather', return_value=sample_weather_sunny):
+        with app.test_client() as client:
+            resp = client.post('/', data={'city': 'fake_city'})
+            assert b"Use sunscreen, dummy!" in resp.data
 
-def test_rainy_weather(mocker, client):
-    mocker.patch('requests.get', return_value=Mock(ok=True))
-    requests.get.return_value.json.return_value = create_mock_weather(20, "Rain")
-    response = client.post('/', data={"city": "test"})
-    assert b"Grab an umbrella!" in response.data
-
-def test_cold_weather(mocker, client):
-    mocker.patch('requests.get', return_value=Mock(ok=True))
-    requests.get.return_value.json.return_value = create_mock_weather(4, "Clear")
-    response = client.post('/', data={"city": "test"})
-    assert b"Put on a jacket!" in response.data
-
-def test_very_cold_weather(mocker, client):
-    mocker.patch('requests.get', return_value=Mock(ok=True))
-    requests.get.return_value.json.return_value = create_mock_weather(-10, "Clear")
-    response = client.post('/', data={"city": "test"})
-    assert b"It's really cold out there." in response.data
-
-def test_misty_weather(mocker, client):
-    mocker.patch('requests.get', return_value=Mock(ok=True))
-    requests.get.return_value.json.return_value = create_mock_weather(20, "Mist")
-    response = client.post('/', data={"city": "test"})
-    assert b"It might be hard to see." in response.data
-
-def test_unrecognized_weather(mocker, client):
-    mocker.patch('requests.get', return_value=Mock(ok=True))
-    requests.get.return_value.json.return_value = create_mock_weather(20, "Unrecognized")
-    response = client.post('/', data={"city": "test"})
-    assert b"I'm unsure" in response.data
+def test_cloudy_weather():
+    with patch('app.get_weather', return_value=sample_weather_cloudy):
+        with app.test_client() as client:
+            resp = client.post('/', data={'city': 'fake_city'})
+            assert b"The shade feels nice." in resp.data
